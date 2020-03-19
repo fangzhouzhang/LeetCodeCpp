8bc077958e71c76c30f3e12dda62c6228d19dc9a
fangzhouzhang <zhangark93@gmail.com>
2020-03-19 11:58:29 -0700

    202003191158

---
+++ _7ReverseInteger.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Fangzhou Zhang on 2020/3/19.
+//
+class Solution {
+public:
+    int reverse(int x1) {
+        bool neg = (x1 >= 0) ? false : true;
+        long x = abs((long)x1);
+        long r = 0;
+        while (x > 0) {
+            int bit = x % 10;
+            r = r * 10 + bit;
+            x /= 10;
+        }
+        if (neg) r = r * -1;
+        if (r < INT_MIN || r > INT_MAX) return 0;
+        return r;
+    }
+};
