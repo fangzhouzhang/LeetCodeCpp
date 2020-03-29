1feb1938ee8986dcfffc4711ecede5d3aae4fbea
fangzhouzhang <zhangark93@gmail.com>
2020-03-28 17:15:19 -0700

    202003281715

--- _65ValidNumber.cpp
+++ _65ValidNumber.cpp
@@ -5,36 +5,32 @@ class Solution {
 public:
     bool isNumber(string s) {
         if (s.size() == 0) return false;
-        while(s[0]==' ')  s.erase(0,1);//delete the  prefix whitespace
-        while(s[s.length()-1]==' ') s.erase(s.length()-1, 1);//delete the suffix whitespace
-        int e_num = 0;
-        int e_pos = 0;
+        int e_pos = -1;
         for (int i = 0; i < s.size(); i++) {
-            if (s[i] == 'e') {
-                e_num++;
-                e_pos = i;
-            } else if (isalpha(s[i])) return false;
+            char c = s[i];
+            if (c == 'e' && e_pos == -1) e_pos = i;
+            else if (c == 'e') return false;
         }
-        if (e_num > 1) return false;
-        if (e_num == 0) {
-            return judge(0, s.size() - 1, s, 1);
-        } else {
-            return judge(0, e_pos - 1, s, 1) && judge(e_pos + 1, s.size() - 1, s, 0);
-        }
-    }
-
-    bool judge(int start, int end, string s, int d) {
-        bool metSign = false;
-        int countD = 0;
-        bool num = false;
-        int i = start;
-        if (s[i] == '-' || s[i] == '+') i++;
-        while (i <= end) {
-            if (s[i] == '.') countD++;
-            else if (isdigit(s[i])) num = true;
-            else return false;
-            i++;
-        }
-        return num && countD <= d;
+        int first = 0, last = s.size() - 1;
+        while (first < s.size() && s[first] == ' ') first++;
+        while (last >= 0 && s[last] == ' ') last--;
+        function<bool(int, int, int)> judge = [&](int start, int end, int count){
+            int dot_count = 0;
+            bool num = false;
+            if (s[start] == '+' || s[start] == '-') start++;
+            while (start <= end) {
+                if (isdigit(s[start])) {
+                    num = true;
+                    start++;
+                } else if (s[start] == '.') {
+                    dot_count++;
+                    if (dot_count > count) return false;
+                    start++;
+                } else return false;
+            }
+            return num;
+        };
+        if (e_pos == -1) return judge(first, last, 1);
+        return judge(first, e_pos - 1, 1) && judge(e_pos + 1, last, 0);
     }
 };
