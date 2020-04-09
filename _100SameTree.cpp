ff324c79c340ad55bcb596dec322a0d98ec586d8
fangzhouzhang <zhangark93@gmail.com>
2020-04-09 14:00:49 -0700

    202004091400

---
+++ _100SameTree.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Fangzhou Zhang on 2020/4/9.
+//
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL && q == NULL) return true;
+        if (p == NULL || q == NULL) return false;
+        if (p->val != q->val) return false;
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right) ;
+    }
+};
