3aa46de5c97be78a80b1de663f6bad0ce33085b0
fangzhouzhang <zhangark93@gmail.com>
2020-04-09 12:53:44 -0700

    202004091253

--- _99RecoverBinarySearchTree.cpp
+++ _99RecoverBinarySearchTree.cpp
@@ -19,4 +19,26 @@ public:
         for (int i = 0; i < nodes.size(); i++) nodes[i]->val = vals[i];
         return;
     }
+
+    void recoverTree1(TreeNode* root) {
+        if (root == NULL) return;
+        TreeNode *pre = NULL;
+        TreeNode *first = NULL;
+        TreeNode *second = NULL;
+        function<void(TreeNode*)> inorder = [&](TreeNode* root){
+            if (root == NULL) return;
+            inorder(root->left);
+            if (pre != NULL && pre->val > root->val) {
+                if (first == NULL) first = pre;
+                second = root;
+            }
+            pre = root;
+            inorder(root->right);
+        };
+        inorder(root);
+        int tmp = first->val;
+        first->val = second->val;
+        second->val = tmp;
+        return;
+    }
 };
