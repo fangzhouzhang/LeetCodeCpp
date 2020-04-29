974e0cd91dd5df270fb834fcae81644a12078c42
fangzhouzhang <zhangark93@gmail.com>
2020-04-28 23:35:37 -0700

    202004282335

--- _1425ConstrainedSubsetSum.cpp
+++ _1425ConstrainedSubsetSum.cpp
@@ -6,32 +6,21 @@ public:
     int constrainedSubsetSum(vector<int>& nums, int k) {
         int n = nums.size();
         if (0 == n) return 0;
-        if (1 == n) return nums[0];
-        int res = nums[0];
-        deque<pair<int,int>> dq;
-        dq.push_front(make_pair(0,nums[0]));
-        for (int i = 1; i < n; i++) {
+        deque<pair<int, int>> dq;
+        int res = INT_MIN;
+        for (int i = 0; i < n; i++) {
             if (!dq.empty()) {
-                if (i - dq.front().first > k) {
-                    dq.pop_front();
-                }
+                if (i - dq.front().first > k) dq.pop_front();
             }
+            int cur_val;
             if (!dq.empty()) {
-                int cur_val = max(0, dq.front().second) + nums[i];
-                res = max(res, cur_val);
-                if (!dq.empty() && dq.front().second < cur_val) {
-                    dq.pop_front();
-                    dq.push_front(make_pair(i, cur_val));
-                } else if (!dq.empty() && dq.front().second >= cur_val && cur_val > dq.back().second) {
-                    while (!dq.empty() && dq.back().second < cur_val) {
-                        dq.pop_back();
-                    }
-                    dq.push_back(make_pair(i, cur_val));
-                } else if (!dq.empty() && dq.back().second >= cur_val) {
-                    dq.push_back(make_pair(i, cur_val));
-                }
-
-            } else dq.push_back(make_pair(i, nums[i]));
+                cur_val = max(0, dq.front().second) + nums[i];
+            } else {
+                cur_val = nums[i];
+            }
+            res = max(res, cur_val);
+            while (!dq.empty() && dq.back().second < cur_val) dq.pop_back();
+            dq.push_back(make_pair(i, cur_val));
         }
         return res;
     }
