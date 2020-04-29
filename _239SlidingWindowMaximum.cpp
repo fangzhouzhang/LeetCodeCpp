fca379eb7c1a91ee5c5289aefcadf24d44446930
fangzhouzhang <zhangark93@gmail.com>
2020-04-28 23:28:07 -0700

    202004282328

--- _239SlidingWindowMaximum.cpp
+++ _239SlidingWindowMaximum.cpp
@@ -5,17 +5,17 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> res;
-        if (nums.size() == 0) return res;
+        if (0 == nums.size()) return res;
         deque<int> dq;
         for (int i = 0; i < nums.size(); i++) {
-            while (dq.size() > 0 && nums[dq.back()] < nums[i]) {
-                dq.pop_back();
+            if (!dq.empty()) {
+                if (i - dq.front() >= k) {
+                    dq.pop_front();
+                }
             }
-            if (dq.size() == 0 || nums[dq.back()] >= nums[i]) dq.push_back(i);
-            if (i + 1 - k >= 0) {
-                res.push_back(nums[dq.front()]);
-                if (dq.size() > 0 && dq.front() <= i + 1 - k) dq.pop_front();
-            }
+            while (!dq.empty() && nums[dq.back()] < nums[i]) dq.pop_back();
+            dq.push_back(i);
+            if (i >= k - 1) res.push_back(nums[dq.front()]);
         }
         return res;
     }
